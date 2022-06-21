6802ce9663b9ecc0a09baf84ce0a4da74dc2845a
mdyamin013 <mdyaminmia2@gmail.com>
2022-06-22 00:44:10 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$('.html-progressbar').rProgressbar({
+    percentage: 100,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.css-progressbar').rProgressbar({
+    percentage: 100,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.bootstrap-progressbar').rProgressbar({
+    percentage: 90,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.php-progressbar').rProgressbar({
+    percentage: 80,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.laravel-progressbar').rProgressbar({
+    percentage: 50,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.mysql-progressbar').rProgressbar({
+    percentage: 90,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.Wordpress').rProgressbar({
+    percentage: 50,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+$('.java-progressbar').rProgressbar({
+    percentage: 40,
+    fillBackgroundColor: '#fc4444',
+   backgroundColor: '#581845',
+       
+});
+$('.jquery-progressbar').rProgressbar({
+    percentage: 80,
+    fillBackgroundColor: '#fc4444',
+    backgroundColor: '#581845',
+       
+});
+//  percentage: null,
+// ShowProgressCount: true,
+// duration: 1000,
+// fillBackgroundColor: '#ed1c24',
+// backgroundColor: '#EEEEEE',
+// borderRadius: '0px',
+// height: '10px',
+// width: '100%'
